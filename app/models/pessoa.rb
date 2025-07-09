67953e4ed1d649cc50e08159850228db86dfd2dd
Elis Rodrigues <elisrb2004@gmail.com>
2025-07-09 17:13:42 -0300

    add configurações do devise

--- app/models/pessoa.rb
+++ app/models/pessoa.rb
@@ -3,4 +3,18 @@ class Pessoa < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  attr_writer :login
+
+  def login
+    @login || self.matricula || self.email
+  end
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    login = conditions.delete(:login)
+    where(conditions).where(
+      ["lower(matricula) = :value OR lower(email) = :value", { value: login.downcase }]
+    ).first
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  # remove sign up
+  devise_for :pessoas, skip: [:registrations]
 end
