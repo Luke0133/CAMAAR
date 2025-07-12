275743dba5a91bc07e405ea4272dedcb37de9aef
José Edson MBS <joseedsonmbs@gmail.com>
2025-07-12 02:36:44 -0300

    update ImportadorSigaa, teste bem-sucedido

--- spec/services/importador_sigaa_spec.rb
+++ spec/services/importador_sigaa_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe ImportadorSIGAA, type: :service do
+RSpec.describe ImportadorSigaa, type: :service do
   describe '#processar' do
     let(:json_data) do
       [
@@ -34,12 +34,28 @@ RSpec.describe ImportadorSIGAA, type: :service do
 
     it 'importa todos os dados corretamente' do
       expect {
-        ImportadorSIGAA.new(json_data).processar
+        ImportadorSigaa.new(json_data).processar
       }.to change(Materia, :count).by(1)
        .and change(Turma, :count).by(1)
        .and change(Pessoa, :count).by(2)
        .and change(Cargo, :count).by(2)
-       .and change(Participantes, :count).by(1)
+       .and change(Participante, :count).by(1)
+
+      materia = Materia.find_by(id: "CIC0001")
+      expect(materia.nome).to eq("OAC")
+
+      professor = Pessoa.find_by(email: "lamar@email.com")
+      expect(professor.nome).to eq("Prof. Lamar")
+
+      aluno = Pessoa.find_by(email: "jose@email.com")
+      expect(aluno.nome).to eq("José")
+
+      turma = Turma.find_by(id_materia: "CIC0001", numero_turma: 1)
+      expect(turma.semestre).to eq("2025.1")
+      expect(turma.professor).to eq("lamar@email.com")
+
+      participante = Participante.find_by(email: "jose@email.com", id_turma: turma.id)
+      expect(participante).to be_present
     end
   end
 end
