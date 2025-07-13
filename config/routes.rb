6aba7f937d5d3dc7a2de037411af6bf05b21b4c6
Luke0133 <lucahmeg@gmail.com>
2025-07-13 20:40:02 -0300

    Update criar formulario

--- config/routes.rb
+++ config/routes.rb
@@ -9,8 +9,8 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-  resources :templates, only: [:index, :show, :edit, :update]
-  resources :formularios, only: [:new, :create, :index]
+
+
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
@@ -22,6 +22,9 @@ Rails.application.routes.draw do
   # root "posts#index"
 
   get "/dashboard", to: "home#index", as: :dashboard
+
+  resources :templates, only: [:index, :show, :edit, :update]
+  resources :formularios, only: [:new, :create, :index]
 
   # remove sign up
   devise_for :pessoas, controllers: {
@@ -38,20 +41,14 @@ Rails.application.routes.draw do
     get "resultados", to: "resultados#index", as: :resultados
     get "resultados/:id/preparar_download", to: "resultados#preparar_download", as: :preparar_download
     get "resultados/:id/download", to: "resultados#download", as: :download_resultado
-  end
-  
-  namespace :admin do
     get "/gerenciamento" => "gerenciamento#index"
-
     post '/gerenciamento/importar', to: 'gerenciamento#importar', as: 'importar_dados'
   end
+ 
   
   namespace :user do
     get 'avaliacoes', to: 'avaliacoes#index'
     resources :formularios, only: [:show]
-  end
-
-  namespace :user do
     resources :avaliacoes, only: [:index] do
       member do
         get :responder
