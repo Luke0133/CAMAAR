599866ebac4edf680c32072017f52c09e66e0988
Elis Rodrigues <elisrb2004@gmail.com>
2025-07-20 17:45:42 -0300

    corrige abc score da passwords controller

--- app/controllers/pessoas/passwords_controller.rb
+++ app/controllers/pessoas/passwords_controller.rb
@@ -13,18 +13,26 @@ class Pessoas::PasswordsController < Devise::PasswordsController
   def update
     self.resource = resource_class.reset_password_by_token(resource_params)
     if resource.errors.empty?
-      resource.unlock_access! if unlockable?(resource)
-      flash[:notice] = "Senha redefinida com sucesso!"
-      sign_in(resource_name, resource)
-      session[:email] = resource.email
-      respond_with resource, location: after_resetting_password_path_for(resource)
+      successful_password_update
     else
-      session[:email] = nil
-      if resource.errors.details[:reset_password_token].present?
-        flash[:alert] = "Token de redefinição de senha é inválido"
-      end
-      respond_with resource
+      failed_password_update
     end
+  end
+
+  def successful_password_update
+    resource.unlock_access! if unlockable?(resource)
+    flash[:notice] = "Senha redefinida com sucesso!"
+    sign_in(resource_name, resource)
+    session[:email] = resource.email
+    respond_with resource, location: after_resetting_password_path_for(resource)
+  end
+
+  def failed_password_update
+    session[:email] = nil
+    if resource.errors.details[:reset_password_token].present?
+      flash[:alert] = "Token de redefinição de senha é inválido"
+    end
+    respond_with resource
   end
 
   protected
